94f590103f1dadf3eb3b26b1f1c7ddc92510e97a
Nick Andrews <nickandrews010@gmail.com>
2023-10-22 19:21:09 -0700

    Created basic login page

---
+++ script.js
@@ -0,0 +1,4 @@
+// Code to redirect to loginpage.html when the button is clicked
+document.getElementById('btnLogin').addEventListener('click', function() {
+    window.location.href = 'loginpage.html';
+});
