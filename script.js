3a9ddce6ea2552be1b73e5680c592c84a15b6ea0
nkozub <143154666+nkozub@users.noreply.github.com>
2023-10-25 14:44:33 -0700

    Update script.js
    adding the feature that take you to a different page depending on your class to the login function

--- script.js
+++ script.js
@@ -1,30 +1,36 @@
-// Code to redirect to loginpage.html when the button is clicked
-document.getElementById('btnLogin').addEventListener('click', function() {
-    window.location.href = 'loginpage.html';
+// Assuming userTable contains a 'userType' field with values 'Mentee' or 'Mentor'
+
+document.getElementById('btnLogin').addEventListener('click', function () {
+    userLogin();
 });
 
 function userLogin() {
-    var userName = document.getElementById('username').value; //grab values from username textbox
-    var userPass = document.getElementById('password').value; //grab values from password textbox
-    var user; //create variable that will hold table information
+    var userName = document.getElementById('username').value;
+    var userPass = document.getElementById('password').value;
+    var user;
 
+    $.get("?tableName=User", function (userTable) {
+        user = JSON.parse(userTable);
 
-        $.get("?tableName=User", function (userTable) {     //JQuery GET method to request an array of information from database
-            //console.log(userTable);
-            user = JSON.parse(userTable);       //converts array data to JSON
+        var checkUser = user.find(function (user) {
+            return user.userName === userName && user.userPass === userPass;
+        });
 
-            var checkUser = user.find(function (user) {         // variable that will use find method to search array for specific values
-                return user.userName === userName && user.userPass === userPass;        //checks whether username and its corresponding password are the same 
-            });
+        if (checkUser) {
+            sessionStorage.setItem("isLoggedIn", true);
+            alert("Login successful!");
 
-            if (checkUser) {
-                sessionStorage.setItem("isLoggedIn", true);
-                alert("Login successful!"); //if both are correlated, success message
-                window.location.href = 'index.html'; // redirect to homepage after successful login
+            // Check the userType and redirect accordingly
+            if (checkUser.userType === 'Mentee') {
+                window.location.href = 'Mentee.html';
+            } else if (checkUser.userType === 'Mentor') {
+                window.location.href = 'Mentor.html';
+            } else {
+                alert("Unknown user type. Please contact support.");
             }
-            else {
-                alert("Your credentials did not match. Try again"); //if both are not correlated, failed message
-            }      
-            }); //end if-else
+        } else {
+            alert("Your credentials did not match. Try again");
+        }
+    });
+}
 
-} //end function
