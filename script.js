1d6b4a9f4748199f55aca416eb4d0a5aa30bf992
clabs123 <142945725+clabs123@users.noreply.github.com>
2023-10-28 19:03:30 -0700

    Troubleshooting

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ document.getElementById('btnLogin').addEventListener('click', function () {
     window.location.href = 'loginpage.html';
 });
 
-document.getElementById('btnLogout').addEventListener('click', function () {
+/* document.getElementById('btnLogout').addEventListener('click', function () {
     var logoutButton = document.getElementById('btnLogout');
     logoutButton.style.display = 'none';
 
     sessionStorage.clear();
     
     window.location.href = 'index.html'; 
-});
+}); */
 
 function userLogin() {
     var userName = document.getElementById('username').value;
