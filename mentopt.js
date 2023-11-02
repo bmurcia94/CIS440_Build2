840e22b8a0ed140e8e67cc7480b55fa500523ec8
nkozub <143154666+nkozub@users.noreply.github.com>
2023-11-02 13:34:01 -0700

    Update mentopt.js

--- mentopt.js
+++ mentopt.js
@@ -53,3 +53,45 @@ function displayMatchResult(match) {
         resultDisplay.textContent = `Matched Mentor: ${match.userName} (${match.userEmail}) with colorType: ${match.colorType}`;
     }
 }
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const listmentee = document.getElementById("listmentee");
+
+    listmentee.addEventListener("click", function() {
+        fetchMentorData().then(mentors => {
+            displayMatchResults(mentors);
+        }).catch(error => {
+            console.error('Error fetching mentor data:', error);
+            displayMatchResults(["Error fetching mentor data."]);
+        });
+    });
+});
+
+function fetchMentorData() {
+    return fetch('/getMentorDetails')
+        .then(response => response.json())
+        .then(data => data)
+        .catch(error => console.error('Error fetching mentor data:', error));
+}
+
+function displayMatchResults(mentors) {
+    const resultDisplay = document.getElementById("mentorMatchResult");
+
+    resultDisplay.style.color = "black";
+    resultDisplay.innerHTML = ''; // Clear the previous content
+
+    if (mentors.length === 0) {
+        resultDisplay.textContent = "No suitable mentors found.";
+    } else {
+        resultDisplay.innerHTML = "Matched Mentors:<br>";
+        mentors.forEach(mentor => {
+            resultDisplay.innerHTML += `<div>Username: ${mentor.userName} (${mentor.userEmail}) with colorType: ${mentor.colorType}</div>`;
+        });
+    }
+}
+
