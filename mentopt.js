fc0dc1e9ac9542d55ee412bef065ed028bd5fb9b
nkozub <143154666+nkozub@users.noreply.github.com>
2023-11-02 15:18:26 -0700

    Update mentopt.js

--- mentopt.js
+++ mentopt.js
@@ -1,24 +1,12 @@
-var menteeData = JSON.parse(sessionStorage.getItem("menteeData"));
-var menteeColorType = menteeData.colorType;
-console.log(menteeColorType)
-// Example data. This will be deleted when this is linked to a database
-// const mentors = [
-//     { id: 'mentor1', colorType: 'red' },
-//     { id: 'mentor2', colorType: 'blue' },
-//     { id: 'mentor3', colorType: 'green' },
-//     { id: 'mentor4', colorType: 'blue' }
-// ];
-
 document.addEventListener("DOMContentLoaded", function() {
-    const findMentorButton = document.getElementById("listmentee");
+    const findMentorButton = document.getElementById("findMentorButton");
 
     findMentorButton.addEventListener("click", function() {
         fetchMentorData().then(mentors => {
-            const match = findRandomMentorForMentee(menteeColorType, mentors);
-            displayMatchResult(match);
+            displayMatchResults(mentors);
         }).catch(error => {
             console.error('Error fetching mentor data:', error);
-            displayMatchResult("Error fetching mentor data.");
+            displayMatchResults(["Error fetching mentor data."]);
         });
     });
 });
@@ -30,26 +18,18 @@ function fetchMentorData() {
         .catch(error => console.error('Error fetching mentor data:', error));
 }
 
-function findRandomMentorForMentee(menteeColorType, mentors) {
-    const matchingMentors = mentors.filter(mentor => mentor.colorType === menteeColorType);
+function displayMatchResults(mentors) {
+    const resultDisplay = document.getElementById("mentorMatchResult");
 
-    if (matchingMentors.length === 0) {
-        return "No suitable mentor found.";
-    }
-
-    const randomIndex = Math.floor(Math.random() * matchingMentors.length);
-    return matchingMentors[randomIndex];
-}
-
-
-function displayMatchResult(match) {
-    const resultDisplay = document.getElementById("mentorMatchResult"); 
-    
     resultDisplay.style.color = "black";
+    resultDisplay.innerHTML = ''; // Clear the previous content
 
-    if (typeof match === 'string') {
-        resultDisplay.textContent = match;
+    if (mentors.length === 0) {
+        resultDisplay.textContent = "No suitable mentors found.";
     } else {
-        resultDisplay.textContent = `Matched Mentor: ${match.userName} (${match.userEmail}) with colorType: ${match.colorType}`;
+        resultDisplay.innerHTML = "Matched Mentors:<br>";
+        mentors.forEach(mentor => {
+            resultDisplay.innerHTML += `<div>Username: ${mentor.userName} (${mentor.userEmail}) with colorType: ${mentor.colorType}</div>`;
+        });
     }
 }
