0451a570287dbf5ab11875e8bb70b2ee7628ec3b
nkozub <143154666+nkozub@users.noreply.github.com>
2023-11-05 22:53:55 -0700

    Update mentopt.js

--- mentopt.js
+++ mentopt.js
@@ -1,24 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const findMentorButton = document.getElementById("listmentee");
-    
-
-    findMentorButton.addEventListener("click", function() {
-        fetchMentorData().then(mentors => {
-            displayMatchResults(mentors);
-        }).catch(error => {
-            console.error('Error fetching mentor data:', error);
-            displayMatchResults(["Error fetching mentor data."]);
-        });
-    });
-});
-
-function fetchMentorData() {
-    return fetch('/getMentorDetails')
-        .then(response => response.json())
-        .then(data => data)
-        .catch(error => console.error('Error fetching mentor data:', error));
-}
-
 function displayMatchResults(mentors) {
     const resultDisplay = document.getElementById("showlist");
     var mentorlist = [];
@@ -31,20 +10,21 @@ function displayMatchResults(mentors) {
     } else {
         resultDisplay.innerHTML = "Matched Mentors:<br>";
         mentors.forEach(mentor => {
-          
-          var newRow = tableBody.insertRow(tableBody.rows.length);
-          var cell1 = newRow.insertCell(0);
-          var cell2 = newRow.insertCell(1);
+            // Create a container div for each mentor
+            const mentorContainer = document.createElement("div");
+            mentorContainer.classList.add("mentor-container");
 
-          cell1.innerHTML = mentor.userName;
-          cell2.innerHTML = '<button class="typeButton">select</button>';
-          
-            //resultDisplay.innerHTML += `<div>Username: ${mentor.userName} (${mentor.userEmail})</div>`;
-            //mentorlist.push({ "Name": mentor.userName, "Email": mentor.userEmail});
+            // Display the mentor's information
+            mentorContainer.innerHTML = `
+                <div>Username: ${mentor.userName} (${mentor.userEmail})</div>
+                <button class="select-button">Select</button>
+                <hr>
+            `;
+
+            // Append the container to the resultDisplay
+            resultDisplay.appendChild(mentorContainer);
+
+            mentorlist.push({ "Name": mentor.userName, "Email": mentor.userEmail });
         });
-        console.log(mentorlist);
-
-
-      
     }
 }
