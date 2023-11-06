753572629f2b217d77746c73f2dd09710f08c552
nkozub <143154666+nkozub@users.noreply.github.com>
2023-11-05 21:20:27 -0700

    Update mentopt.js

--- mentopt.js
+++ mentopt.js
@@ -31,16 +31,8 @@ function displayMatchResults(mentors) {
     } else {
         resultDisplay.innerHTML = "Matched Mentors:<br>";
         mentors.forEach(mentor => {
-          
-          var newRow = tableBody.insertRow(tableBody.rows.length);
-          var cell1 = newRow.insertCell(0);
-          var cell2 = newRow.insertCell(1);
-
-          cell1.innerHTML = row.userName;
-          cell2.innerHTML = '<button class="typeButton">select</button>';
-          
-            //resultDisplay.innerHTML += `<div>Username: ${mentor.userName} (${mentor.userEmail})</div>`;
-            //mentorlist.push({ "Name": mentor.userName, "Email": mentor.userEmail});
+            resultDisplay.innerHTML += `<div>Username: ${mentor.userName} (${mentor.userEmail}) with colorType: ${mentor.colorType}</div>`;
+            mentorlist.push({ "Name": mentor.userName, "Email": mentor.userEmail});
         });
         console.log(mentorlist);
-
+    }
