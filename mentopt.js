69a50c9e68c5c9ec0b4caaeaa7c2ab62652fc555
clabs123 <142945725+clabs123@users.noreply.github.com>
2023-11-06 16:50:29 -0700

    Edit format and button

--- mentopt.js
+++ mentopt.js
@@ -21,6 +21,37 @@ function fetchMentorData() {
 
 function displayMatchResults(mentors) {
     const resultDisplay = document.getElementById("showlist");
+    resultDisplay.style.color = "black";
+    resultDisplay.innerHTML = ''; // Clear the previous content
+
+    if (mentors.length === 0) {
+        resultDisplay.textContent = "No suitable mentors found.";
+    } else {
+        resultDisplay.innerHTML = "Matched Mentors:<br>";
+        const table = document.createElement("table");
+
+        mentors.forEach(mentor => {
+            const row = table.insertRow();
+            const cell1 = row.insertCell(0);
+            const cell2 = row.insertCell(1);
+            const buttonCell = row.insertCell(2);
+
+            cell1.textContent = mentor.userName;
+            cell2.textContent = mentor.userEmail;
+
+            const selectButton = document.createElement("button");
+            selectButton.className = "typeButton";
+            selectButton.textContent = "Select Mentor";
+            buttonCell.appendChild(selectButton);
+        });
+
+        resultDisplay.appendChild(table);
+    }
+}
+
+/* 
+function displayMatchResults(mentors) {
+    const resultDisplay = document.getElementById("showlist");
     var mentorlist = [];
 
     resultDisplay.style.color = "black";
@@ -31,13 +62,10 @@ function displayMatchResults(mentors) {
     } else {
         resultDisplay.innerHTML = "Matched Mentors:<br>";
         mentors.forEach(mentor => {
-            resultDisplay.innerHTML += `
-                <div>Name: ${mentor.userName}</div>
-                <div>Email: ${mentor.userEmail}</div>
-                <button class="typeButton">Select Mentor</button>
-            `;
-            mentorlist.push({ "Name": mentor.userName, "Email": mentor.userEmail });
+            resultDisplay.innerHTML += `<div>Username: ${mentor.userName} (${mentor.userEmail}) with colorType: ${mentor.colorType}</div>`;
+            mentorlist.push({ "Name": mentor.userName, "Email": mentor.userEmail});
         });
         console.log(mentorlist);
     }
 }
+*/
