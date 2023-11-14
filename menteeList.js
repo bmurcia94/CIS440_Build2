3d5087833de2e1a658ed3dae440fd8997cc9cd4e
bryant.murcia <143425870+bmurcia94@users.noreply.github.com>
2023-11-14 02:22:42 -0700

    Update menteeList.js

--- menteeList.js
+++ menteeList.js
@@ -31,11 +31,34 @@ function displayMatchResults(mentee) {
     if (mentee.length === 0) {
         resultDisplay.textContent = "You have no mentees.";
     } else {
-        resultDisplay.innerHTML = "<hr>Your Mentees:<br>";
+        resultDisplay.innerHTML = "<hr><br><div class='mentees-header'>Your Mentees:</div><br>";
+
+        const table = document.createElement("table");
+
+        const headerRow = table.insertRow();
+        const header1 = headerRow.insertCell(0);
+        const header2 = headerRow.insertCell(1);
+        const header3 = headerRow.insertCell(2);
+   
+        header1.textContent = "First Name";
+        header2.textContent = "Last Name";
+        header3.textContent = "Email";
+       
         mentee.forEach(mentee => {
-            resultDisplay.innerHTML += `<div>Name: ${mentee.userFirst} ${mentee.userLast} - Email: ${mentee.userEmail}</div>`;
-            menteelist.push({ "First": mentee.userFirst, "Last": mentee.userLast, "Email": mentee.userEmail});
+            // resultDisplay.innerHTML += `<div>Name: ${mentee.userFirst} ${mentee.userLast} - Email: ${mentee.userEmail}</div>`;
+            // menteelist.push({ "First": mentee.userFirst, "Last": mentee.userLast, "Email": mentee.userEmail});
+
+            const row = table.insertRow();
+            const cell1 = row.insertCell(0);
+            const cell2 = row.insertCell(1);
+            const cell3 = row.insertCell(2);
+
+            cell1.textContent = mentee.userFirst;
+            cell2.textContent = mentee.userLast;
+            cell3.textContent = mentee.userEmail;
+             
         });
         //console.log(menteelist);
+        resultDisplay.appendChild(table);
     }
 }
